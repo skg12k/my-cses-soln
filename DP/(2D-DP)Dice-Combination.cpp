a1520d1965957ce396fbda2b233d2a9dbbb65a2c
Sanatan Kumar Gupta <workforskg@gmail.com>
2025-06-18 13:41:13 +0530

    removed template stuff

--- DP/(2D-DP)Dice-Combination.cpp
+++ DP/(2D-DP)Dice-Combination.cpp
@@ -1,41 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const ll inf = 1e18;
-const ll MAXN = 3e5;
-#define rep(i, a, b) for (ll i = a; i < b; i++)
-#define rrep(i, a, b) for (ll i = a; i >= b; i--)
-#define f(i, n) for (ll i = 0; i < n; ++i)
-#define input(v, n)            \
-    for (ll i = 0; i < n; i++) \
-    {                          \
-        cin >> v[i];           \
-    }
-ll i;
-#define loop(n) for (i = 0; i < n; i++)
-#define sz(x) (int)(x).size()
-#define ff first
-#define ss second
-#define endl '\n'
-#define all(v) (v).begin(), v.end()
-#define vi vector<ll>
-#define pii pair<ll, ll>
-#define vpi vector<pii>
-#define vb vector<bool>
-#define pb push_back
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-#define dbg(x) cerr << #x << " = " << x << endl;
-#define dbgv(v)             \
-    cerr << #v << " = [ ";  \
-    for (auto &ele : v)     \
-        cerr << ele << " "; \
-    cerr << "]" << endl;
-ll MOD = 1e9 + 7;
-ll MOD2 = 998244353;
-#define fastio               \
-    ios::sync_with_stdio(0); \
-    cin.tie(0);
+#define vi vector<int>
 
 void solve()
 {
@@ -56,12 +21,5 @@ void solve()
 }
 int main()
 {
-
-    fastio;
-    int tt = 1;
-    // cin >> tt;
-    while (tt--)
-    {
-        solve();
-    }
+    solve();
 }
